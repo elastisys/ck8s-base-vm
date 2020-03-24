7b5f2311afe208c3fc96e8eff010dfee542f8979
Lennart Jern <lennart.jern@elastisys.com>
2020-03-24 12:18:15 +0200

    Set kubernetes version

--- scripts/install-k8s-tools.sh
+++ scripts/install-k8s-tools.sh
@@ -2,6 +2,8 @@
 
 set -e -x
 
+version="1.15.11-00"
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
@@ -9,5 +11,5 @@ deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet-${version} kubeadm-${version} kubectl-${version}
 apt-mark hold kubelet kubeadm kubectl
