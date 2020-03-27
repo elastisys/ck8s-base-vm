721be566d379d376c71ac8e009cf3b1fbc5d8c7c
Lennart Jern <lennart.jern@elastisys.com>
2020-03-27 08:02:56 +0200

    Remove superfluous sudo

--- scripts/install-k8s-tools.sh
+++ scripts/install-k8s-tools.sh
@@ -4,7 +4,7 @@ set -e -x
 
 version="1.15.11-00"
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
