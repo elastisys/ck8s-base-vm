d2adebad9df3f33efe236a1828975ffa334ad85e
Simon Kollberg <simonkollberg@gmail.com>
2020-04-16 18:15:41 +0200

    Move logic from Makefile to main.sh

--- Makefile
+++ Makefile
@@ -3,38 +3,14 @@ ROOT_PATH := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 all: build test
 
 build:
-	PACKER_LOG=1 CHECKPOINT_DISABLE=1 \
-	packer build  -var-file $(ROOT_PATH)variables.json \
-		$(ROOT_PATH)baseos.json > $(ROOT_PATH)baseos-build.log; \
-		case "$$?" in \
-			0) \
-				echo "BaseOS image created." \
-			;; \
-			*) \
-				echo "Error building baseOS image."; \
-				echo "Check: $(ROOT_PATH)baseos-build.log"; \
-				exit 1 \
-			;; \
-		  esac
+	PACKER_LOG=1 CHECKPOINT_DISABLE=1 $(ROOT_PATH)main.sh build
 
 test:
-	PACKER_LOG=1 CHECKPOINT_DISABLE=1 \
-	packer build -var-file $(ROOT_PATH)variables.json \
-		$(ROOT_PATH)baseos-test.json > $(ROOT_PATH)baseos-build-tests.log; \
-		case "$$?" in \
-			0) \
-				echo "Test finished successfully." \
-			;; \
-			*) \
-				echo "Error while testing baseOS image."; \
-				echo "Check: $(ROOT_PATH)baseos-build-test.log"; \
-				exit 1 \
-			;; \
-		  esac
+	PACKER_LOG=1 CHECKPOINT_DISABLE=1 $(ROOT_PATH)main.sh test
 
 clean:
 	rm -rf $(ROOT_PATH)output-baseos*
-	rm -f $(ROOT_PATH)baseos-build*.log
+	rm -f $(ROOT_PATH)baseos*.log
 	rm -rf $(ROOT_PATH)packer_cache
 
 .PHONY: build test clean
---
+++ main.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -u
+
+here="$(dirname "$(readlink -f "$0")")"
+
+var_file="${here}/variables.json"
+
+build_config="${here}/baseos.json"
+build_log="${here}/baseos.log"
+
+test_config="${here}/baseos-test.json"
+test_log="${here}/baseos-test.log"
+
+usage() {
+    echo "Usage: ${0} <build|test>" >&2
+    exit 1
+}
+
+[ ${#} -eq 1 ] || usage
+
+case "${1}" in
+    build)
+        packer build -var-file "${var_file}" "${build_config}" > "${build_log}"
+
+        case "${?}" in
+            0)
+                echo -e "\e[32mBaseOS image created.\e[0m" >&2
+            ;;
+            *)
+                echo -e "\e[31mError building baseOS image.\e[0m" >&2
+                echo "Check: ${build_log}" >&2
+                exit 1
+            ;;
+        esac
+    ;;
+    test)
+        packer build -var-file "${var_file}" "${test_config}" > "${test_log}"
+
+        case "${?}" in
+            0)
+                echo -e "\e[32mTest finished successfully.\e[0m" >&2
+            ;;
+            *)
+                echo -e "\e[31mError while testing baseOS image.\e[0m" >&2
+                echo "Check: ${baseos_test_log}" >&2
+                exit 1
+            ;;
+        esac
+    ;;
+    *) usage ;;
+esac
